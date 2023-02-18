7a87fd783b4dd67ef9e063866c65d454bfa5f736
qingyang0506 <1565134797qq@gmail.com>
2023-02-18 21:09:16 +1300

    config github pages

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/admin-dashboard-template">
       <App />
     </BrowserRouter>
   </React.StrictMode>
